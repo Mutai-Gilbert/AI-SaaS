035760061cfbbd2d5b98b68a88374e2f752ec2a2
mutai-gilbert <mutaigilbert85@gmail.com>
2024-01-24 12:58:11 +0300

    Add new changes

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -1,11 +1,21 @@
 "use client";
+import { useEffect, useState } from "react";
 import { Button } from "@/components/ui/button";
-
 import { Menu } from "lucide-react";
-import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import Sidebar from "@/components/sidebar";
 
+
 const MobileSidebar = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+  if (!isMounted) {
+    return null;
+  }
+
   return (
     <Sheet>
       <SheetTrigger>
