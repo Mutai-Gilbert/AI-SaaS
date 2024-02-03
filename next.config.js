d6bfaa13ab17b49cac34b1dc654cc2f1321dc768
mutai-gilbert <mutaigilbert85@gmail.com>
2024-02-03 09:49:37 +0300

    Add new changes

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: [''],
+        domains: [
+            'oaidalleapipprodscus.blob.core.windows.net',
+        ]    
     }
 }
 
