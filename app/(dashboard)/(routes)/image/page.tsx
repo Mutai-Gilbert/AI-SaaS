ea2b6806c0ebbf5d18b1dccbc24263dc88240d5b
mutai-gilbert <mutaigilbert85@gmail.com>
2024-02-03 09:36:20 +0300

    Add new changes

--- app/(dashboard)/(routes)/image/page.tsx
+++ app/(dashboard)/(routes)/image/page.tsx
@@ -16,7 +16,7 @@ import { Empty } from "@/components/empty";
 import { Loader } from "@/components/loader";
 import { cn } from "@/lib/utils";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
-import { Card, CardHeader } from "@/components/ui/card";
+import { Card, CardFooter } from "@/components/ui/card";
 
 const ImagePage = () => {
     const router = useRouter();
@@ -39,7 +39,6 @@ const ImagePage = () => {
             const response = await axios.post("/api/image", values);
             const urls = response.data.map((image : {url: string}) => image.url);
             setImages(urls);
-            form.reset();
         } catch (error: any) {
             //TODO: Open pro Modal
             console.log("error");
@@ -50,19 +49,19 @@ const ImagePage = () => {
 
     return (
         <div>
-            <Heading
-                title="Image Generation"
-                description="Turn your prompt to an image"
-                icon={ImageIcon}
-                iconColor="text-pink-500"
-                bgColor="bg-pink-500/10"
-            />
+                <Heading
+                    title="Image Generation"
+                    description="Turn your prompt to an image"
+                    icon={ImageIcon}
+                    iconColor="text-pink-700"
+                    bgColor="bg-pink-700/10"
+                />
 
-            <div className="px-4 lg:px-8">
-                <Form {...form}>
-                    <form
-                        onSubmit={form.handleSubmit(onSubmit)}
-                        className="rounded-lg border
+                <div className="px-4 lg:px-8">
+                    <Form {...form}>
+                        <form
+                            onSubmit={form.handleSubmit(onSubmit)}
+                            className="rounded-lg border
             w-full
             p-4
             px-3
@@ -71,131 +70,127 @@ const ImagePage = () => {
             grid
             grid-cols-12
             gap-2"
-                    >
-                        <FormField
-                            name="prompt"
-                            render={({ field }) => (
-                                <FormItem className="col-span-12 lg:col-span-6">
-                                    <FormControl className="m-0 p-0">
-                                        <Input
-                                            className="border-0 outline-none
+                        >
+                            <FormField
+                                name="prompt"
+                                render={({ field }) => (
+                                    <FormItem className="col-span-12 lg:col-span-6">
+                                        <FormControl className="m-0 p-0">
+                                            <Input
+                                                className="border-0 outline-none
                       focus-visible:ring-0
                       focus-visible:ring-transparent"
+                                                disabled={isLoading}
+                                                placeholder="A pciture of a horse in Swiss Alps"
+                                                {...field}
+                                            />
+                                        </FormControl>
+                                    </FormItem>
+                                )}
+                            />
+                            <FormField
+                                control={form.control}
+                                name="amount"
+                                render={({ field }) => (
+                                    <FormItem className="col-span-12 lg:col-span-2">
+                                        <Select
                                             disabled={isLoading}
-                                            placeholder="A pciture of a horse in Swiss Alps"
-                                            {...field}
-                                        />
-                                    </FormControl>
-                                </FormItem>
-                            )}
-                        />
-                        <FormField
-                         control= {form.control}
-                         name="amount"
-                         render={({field}) => (
-                            <FormItem className="col-span-12 lg:col-span-2">
-                                <Select
-                                 disabled={isLoading}
-                                 onValueChange={field.onChange}
-                                 value={field.value}
-                                 defaultValue={field.value}>
-                                   <FormControl>
-                                       <SelectTrigger>
-                                           <SelectValue defaultValue={field.value} />
-                                       </SelectTrigger>
-                                       <SelectContent>
-                                           {amountOptions.map((option) => (
-                                               <SelectItem
-                                                   key={option.value}
-                                                   value={option.value}
-                                               >
-                                                   {option.label}
-                                               </SelectItem>
-                                           ))}
-                                       </SelectContent>
-                                   </FormControl>
-                                </Select>
-                            </FormItem>
-                         )}
-                        />
-                             <FormField
-                         control= {form.control}
-                         name="resolution"
-                         render={({field}) => (
-                            <FormItem className="col-span-12 lg:col-span-2">
-                                <Select
-                                 disabled={isLoading}
-                                 onValueChange={field.onChange}
-                                 value={field.value}
-                                 defaultValue={field.value}>
-                                   <FormControl>
-                                       <SelectTrigger>
-                                           <SelectValue defaultValue={field.value} />
-                                       </SelectTrigger>
-                                       <SelectContent>
-                                           {resolutionOptions.map((option) => (
-                                               <SelectItem
-                                                   key={option.value}
-                                                   value={option.value}
-                                               >
-                                                   {option.label}
-                                               </SelectItem>
-                                           ))}
-                                       </SelectContent>
-                                   </FormControl>
-                                </Select>
-                            </FormItem>
-                         )}
-                        />
-                        <Button className="col-span-12 lg:col-span-2 w-full" disabled={isLoading}>
-                            Generate
-                        </Button>
+                                            onValueChange={field.onChange}
+                                            value={field.value}
+                                            defaultValue={field.value}>
+                                            <FormControl>
+                                                <SelectTrigger>
+                                                    <SelectValue defaultValue={field.value} />
+                                                </SelectTrigger>
+                                            </FormControl>
+                                                <SelectContent>
+                                                    {amountOptions.map((option) => (
+                                                        <SelectItem
+                                                            key={option.value}
+                                                            value={option.value}
+                                                        >
+                                                            {option.label}
+                                                        </SelectItem>
+                                                    ))}
+                                                </SelectContent>
+                                        </Select>
+                                    </FormItem>
+                                )}
+                            />
+                            <FormField
+                                control={form.control}
+                                name="resolution"
+                                render={({ field }) => (
+                                    <FormItem className="col-span-12 lg:col-span-2">
+                                        <Select
+                                            disabled={isLoading}
+                                            onValueChange={field.onChange}
+                                            value={field.value}
+                                            defaultValue={field.value}>
+                                            <FormControl>
+                                                <SelectTrigger>
+                                                    <SelectValue defaultValue={field.value} />
+                                                </SelectTrigger>
+                                            </FormControl>
+                                                <SelectContent>
+                                                    {resolutionOptions.map((option) => (
+                                                        <SelectItem
+                                                            key={option.value}
+                                                            value={option.value}
+                                                        >
+                                                            {option.label}
+                                                        </SelectItem>
+                                                    ))}
+                                                </SelectContent>
+                                            
+                                        </Select>
+                                    </FormItem>
+                                )}
+                            />
+                            <Button className="col-span-12 lg:col-span-2 w-full" disabled={isLoading}>
+                                Generate
+                            </Button>
 
-                    </form>
-                </Form>
-            </div>
-
-            <div className="space-y-4 mt-4">
-                {isLoading && (
-                    <div className="p-20">
-                        <Loader />
-                    </div>
-                )}
-                {images.length == 0 && !isLoading && (
-                        <Empty label="No conversation generated" />
-                )}
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grif-cols-3 xl:grid-cols-4 gap-4 mt-8">
-                    {images.map((src) => (
-                        <Card
-                        key={src}
-                        className="rounded-lg overflow-hidden"
-                        >
-                            <div className="relative aspect-square">
-                                <Image
-                                alt="Image"
-                                fill
-                                src={src}
-                                 />
-                            </div>
-                            <CardHeader className="p-2">
-                                <Button 
-                                onClick={() => window.open(src)} 
-                                variant="secondary" 
-                                className="w-full">
-                                    <Download className="h-4 w-4 mr-2"/>
-                                </Button>
-                            </CardHeader>
-                        </Card>
-                    ))}
+                        </form>
+                    </Form>
                 </div>
-            </div>
+
+                <div className="space-y-4 mt-4">
+                    {isLoading && (
+                        <div className="p-20">
+                            <Loader />
+                        </div>
+                    )}
+                    {images.length == 0 && !isLoading && (
+                        <Empty label="No Images generated" />
+                    )}
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grif-cols-3 xl:grid-cols-4 gap-4 mt-8">
+                        {images.map((src) => (
+                            <Card
+                                key={src}
+                                className="rounded-lg overflow-hidden"
+                            >
+                                <div className="relative aspect-square">
+                                    <Image
+                                        fill
+                                        alt="Image"
+                                        src={src}
+                                    />
+                                </div>
+                                <CardFooter className="p-2">
+                                    <Button
+                                        onClick={() => window.open(src)}
+                                        variant="secondary"
+                                        className="w-full">
+                                        <Download className="h-4 w-4 mr-2" />
+                                    </Button>
+                                </CardFooter>
+                            </Card>
+                        ))}
+                    </div>
+                </div>
         </div>
     );
 };
-
-
-
-
-
 
 export default ImagePage;
