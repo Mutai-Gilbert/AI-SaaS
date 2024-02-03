7b2402661d3627e67aa976122f90cad8d5a5b856
mutai-gilbert <mutaigilbert85@gmail.com>
2024-02-03 09:37:04 +0300

    Add new changes

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,17 +1,14 @@
 import { UserButton } from "@clerk/nextjs";
-import { ClerkProvider } from "@clerk/nextjs";
 import MobileSidebar from "@/components/mobile-sidebar";
 
 const Navbar = () => {
   return (
-    <ClerkProvider>
       <div className="flex items-center p-4">
         <MobileSidebar />
         <div className="flex w-full justify-end">
           <UserButton afterSignOutUrl="/" />
         </div>
       </div>
-    </ClerkProvider>
   );
 };
 
